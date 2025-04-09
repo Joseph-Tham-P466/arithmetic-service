fbbcb51baff5583b81394a6deeffebfca5b05578
jtham <josephtham03@gmail.com>
2025-04-09 19:55:18 -0400

    updating

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 set -u
-: "$CONTAINER_REGISTRY"
-: "$VERSION"
 
-envsubst < ./deploy-arithmetica.yaml | kubectl apply -f -
+# Check if required environment variables are set
+if [ -z "$CONTAINER_REGISTRY" ] || [ -z "$VERSION" ]; then
+    echo "Error: CONTAINER_REGISTRY and VERSION must be set"
+    exit 1
+fi
+
+# Use sed to replace the variables in the deployment file
+sed "s|\$CONTAINER_REGISTRY|$CONTAINER_REGISTRY|g; s|\$VERSION|$VERSION|g" ./deploy-arithmetica.yaml | kubectl apply -f -
