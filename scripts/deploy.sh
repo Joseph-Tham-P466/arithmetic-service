4804a12e16a466ed009d938e17534496356c06d0
jtham <josephtham03@gmail.com>
2025-04-09 20:08:33 -0400

    updating

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,4 @@ if [ -z "$CONTAINER_REGISTRY" ] || [ -z "$VERSION" ]; then
 fi
 
 # Use sed to replace the variables in the deployment file
-sed "s|\$CONTAINER_REGISTRY|$CONTAINER_REGISTRY|g; s|\$VERSION|$VERSION|g" ./deploy-arithmetica.yaml | kubectl apply -f -
+sed "s|\$CONTAINER_REGISTRY|$CONTAINER_REGISTRY|g; s|\$VERSION|$VERSION|g" ./kubernetes/deploy-arithmetica.yaml | kubectl apply -f -
