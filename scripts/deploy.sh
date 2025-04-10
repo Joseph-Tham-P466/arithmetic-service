8ffd65e1e80b7799e01434555efba1830a7516a5
jtham <josephtham03@gmail.com>
2025-04-09 20:10:17 -0400

    updating

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,8 @@ if [ -z "$CONTAINER_REGISTRY" ] || [ -z "$VERSION" ]; then
     exit 1
 fi
 
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # Use sed to replace the variables in the deployment file
-sed "s|\$CONTAINER_REGISTRY|$CONTAINER_REGISTRY|g; s|\$VERSION|$VERSION|g" ./kubernetes/deploy-arithmetica.yaml | kubectl apply -f -
+sed "s|\$CONTAINER_REGISTRY|$CONTAINER_REGISTRY|g; s|\$VERSION|$VERSION|g" "$SCRIPT_DIR/kubernetes/deploy-arithmetica.yaml" | kubectl apply -f -
